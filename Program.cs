23a55f3359b9899fa3476208c4bdef8dbc6aad73
Souksamlane Hugo <37270994+HSouksamlane@users.noreply.github.com>
2022-02-18 17:52:44 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Project_ST2TRD_SOUKSAMLANE_LEXTRAIT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
